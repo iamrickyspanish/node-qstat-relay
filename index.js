a15daa4a4ff4bb8ee735a8150c194584f517c26e
Enrico <enrico@hrlab.de>
2022-02-14 23:17:48 +0100

    add qstat config and micro cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { sendError, send, createError, json } = require("micro");
 const { exec } = require("child_process");
+const cors = require("micro-cors")();
 
 const runCmd = async (serverType, host) => {
   return new Promise((resolve, reject) => {
@@ -15,10 +16,12 @@ const runCmd = async (serverType, host) => {
   });
 };
 
-module.exports = async (req, res) => {
+module.exports = cors(async (req, res) => {
   try {
     if (req.method === "GET") {
       const { game, host } = await json(req);
+
+      if (!game || !host) throw "nigger";
       response = await runCmd(game, host);
       return send(res, 200, `${response}`);
     }
@@ -26,4 +29,4 @@ module.exports = async (req, res) => {
   } catch (e) {
     sendError(req, res, e);
   }
-};
+});
