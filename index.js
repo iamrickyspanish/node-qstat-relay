5e4e63390abde52e33a162bc2f29029b5073a975
Enrico <enrico@hrlab.de>
2022-02-19 19:51:00 +0100

    simple json formatting

--- index.js
+++ index.js
@@ -15,12 +15,35 @@ const runCmd = async (serverType, host) => {
   });
 };
 
+const formatResponse = (response) => {
+  const [host, playerNumbers, botNumbers, map, responseTime, name] = response
+    .slice(response.indexOf("\n") + 1) // skip first line
+    .split("  ")
+    .filter((elm) => elm !== "")
+    .map((elm) => elm.trim());
+
+  const [players, maxPlayers] = playerNumbers.split("/");
+  const [bots, maxBots] = botNumbers.split("/");
+
+  const formattedResponse = {
+    host,
+    players,
+    maxPlayers,
+    bots,
+    maxBots,
+    map,
+    responseTime,
+    name
+  };
+  return formattedResponse;
+};
+
 module.exports = cors(async (req, res) => {
   try {
     if (req.method === "GET") {
-      const { game, host } = await json(req);
-      const response = await runCmd(game, host);
-      return send(res, 200, `${response}`);
+      const { type, host } = await json(req);
+      const response = await runCmd(type, host);
+      return send(res, 200, formatResponse(response));
     }
     throw createError(400, "invalid format");
   } catch (e) {
